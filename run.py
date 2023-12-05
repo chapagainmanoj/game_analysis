fc0fec09671070fc5baf2ed9441e6c65a6123e78
Manoj <chapagainmanoj35@gmail.com>
2023-12-04 23:41:16 -0500

    fix the bug

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from code.match import Match
 
 if __name__ == "__main__":
-    # first_game = Match(path="./data/*")
-    first_game = Match()
+    first_game = Match(path="./data/*")
     first_game.load_game()
